19bd33e3f2a030522b4f9704bc764660a264b59b
Aleksandr Karpan <aleksandrkarpan7@gmail.com>
2016-10-25 00:08:38 +0300

    second commit

--- index.php
+++ index.php
@@ -2,37 +2,41 @@
 <html>
 <head>
   <link charset="utf-8">
-    <link rel="stylesheet" href="css/bootstrap.min.css">
+       <link rel="stylesheet" href="css/bootstrap.min.css">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
-  <title>Fawell first blog</title>
+    <title>Fawell first blog</title>
 </head>
 <body>
+<header>
+    <h1>Blog for news</h1>
+</header>
+
+<section>
 
 <?php
 
-$ connection = mysql_connect("localhost", "db_user", "admin123");
-$db = mysql_select_db("my_db");
-mysql_set_charset("utf-8");
+$connection = mysqli_connect("localhost", "db_user", "admin123");
 
-if(!$connection || !db)
+$db = mysqli_select_db("my_db");
+mysqli_set_charset("utf-8");
+
+if(!$connection || !$db)
 {
-	exit(mysql_error());
-	
+	exit(mysqli_error());
 }
 
-$result = mysql_query(" 
+$result = mysqli_query(" 
                        SELECT * FROM news
 					   ORDER BY id DESC
                        ");
 
-mysql_close();
+mysqli_close();
 
-while($row = mysql_fetch_assoc($result)){
+while($row = mysqli_fetch_array($result)){
 	echo $row ['title']."<br>";
 }  
 ?>
-<?php
+
 <h1><?php $row['title']?></h1>
 <p><?php $row['text']?></p>
 <p><?php $row['img']?></p>
@@ -40,6 +44,8 @@ while($row = mysql_fetch_assoc($result)){
 <p>Автор новости:<?php $row['author']?></p> 
 <hr/>
 
-?>
+
+</section>
+
 </body>
 </html>
